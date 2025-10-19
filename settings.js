fbc17efde9c98187141a3013ca985446a3b9122f
clementinepeter00-source <clementinepeter00@gmail.com>
2025-10-19 22:22:49 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Morgan', // Your name
-  ownerNumber: '2348177123102', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
